8fc7dd756172abfe37c1642939e3e35192976a60
Mike <sales@rfdprint.com>
2019-01-25 20:37:32 -0600

    Finised Responsive Code

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,6 +3,9 @@ import RevealOnScroll from './modules/RevealOnScroll';
 import $ from "jquery";
 import Modal from "./modules/Modal";
 import Tabs from "./modules/Tabs";
+//import React from 'react';
+//import YouTube from 'react-youtube';
+//import ReactYouTube from "./modules/YouTube"
 
 
 var tabs = new Tabs();
@@ -10,3 +13,4 @@ var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var modal = new Modal();
+//var youtube = new  ReactYouTube();
--- app/temp/scripts/Vendor.js
+++ app/temp/scripts/Vendor.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 7);
+/******/ 	return __webpack_require__(__webpack_require__.s = 8);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -71,20 +71,21 @@
 /* 4 */,
 /* 5 */,
 /* 6 */,
-/* 7 */
+/* 7 */,
+/* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(8);
-
 __webpack_require__(9);
 
 __webpack_require__(10);
 
+__webpack_require__(11);
+
 /***/ }),
-/* 8 */
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var __WEBPACK_AMD_DEFINE_RESULT__;/*! picturefill - v3.0.2 - 2016-02-12
@@ -1635,7 +1636,7 @@ var __WEBPACK_AMD_DEFINE_RESULT__;/*! picturefill - v3.0.2 - 2016-02-12
 
 
 /***/ }),
-/* 9 */
+/* 10 */
 /***/ (function(module, exports) {
 
 (function(window, factory) {
@@ -2338,7 +2339,7 @@ var __WEBPACK_AMD_DEFINE_RESULT__;/*! picturefill - v3.0.2 - 2016-02-12
 
 
 /***/ }),
-/* 10 */
+/* 11 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
